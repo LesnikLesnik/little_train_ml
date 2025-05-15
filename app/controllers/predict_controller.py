44b0a8a8a37b4fa0d9a4f4a5b8aee7cb8cac9300
LesnikLesnik <krolander02@gmail.com>
2025-05-15 10:46:58 +0300

    добавлена логика для загрузки pkl файла с обученной моделью с яндекс диска

--- app/controllers/predict_controller.py
+++ app/controllers/predict_controller.py
@@ -1,31 +1,58 @@
 from flask import Blueprint, render_template, request, redirect, url_for, current_app
-import joblib
 import os
+import joblib
+import pandas as pd
 from ml.model.ensure_model import MODEL_PATH
 
-predict_bp = Blueprint("predict", __name__, url_prefix="/predict")
+predict_bp = Blueprint('predict', __name__, url_prefix="/predict", template_folder='../templates')
 
-def get_model():
+def load_model():
+    """Ленивая загрузка модели из файла"""
     if current_app.config["ml_model"]["model"] is None:
         if os.path.exists(MODEL_PATH):
-            current_app.config["ml_model"]["model"] = joblib.load(MODEL_PATH)
-    return current_app.config["ml_model"]["model"]
+            model_data = joblib.load(MODEL_PATH)
+            current_app.config["ml_model"]["model"] = model_data['model']
+            current_app.config["ml_model"]["feature_columns"] = model_data['feature_columns']
+
+def get_model_and_features():
+    load_model()
+    model = current_app.config["ml_model"].get("model")
+    features = current_app.config["ml_model"].get("feature_columns")
+    return model, features
 
 @predict_bp.route("/form")
 def form():
-    model = get_model()
+    model, _ = get_model_and_features()
     if model is None:
         return redirect(url_for("model_loader.loading_page"))
     return render_template("form.html")
 
-@predict_bp.route("/submit", methods=["POST"])
-def submit():
-    model = get_model()
+# TODO пофиксить роутинг /predict/predict
+@predict_bp.route("/predict", methods=["GET", "POST"])
+def predict():
+    model, feature_columns = get_model_and_features()
     if model is None:
         return redirect(url_for("model_loader.loading_page"))
 
-    # Пример простой логики предсказания
-    input_data = request.form.get("input_text", "")
-    prediction = model.predict([input_data])[0]
+    try:
+        data = request.form.to_dict()
+        input_data = pd.DataFrame([data])
 
-    return render_template("result.html", prediction=prediction)
+        numeric_fields = [
+            'Age', 'Academic Pressure', 'Work Pressure', 'CGPA',
+            'Study Satisfaction', 'Job Satisfaction', 'Work/Study Hours',
+            'Financial Stress'
+        ]
+        for field in numeric_fields:
+            input_data[field] = pd.to_numeric(input_data[field])
+
+        input_data = input_data[feature_columns]
+
+        prediction = int(model.predict(input_data)[0])
+        probability = float(model.predict_proba(input_data)[0][1])
+
+        return render_template("result.html", prediction=prediction, probability=probability)
+
+    except Exception as e:
+        print(e.__cause__)
+        return render_template("error.html", error=str(e))
